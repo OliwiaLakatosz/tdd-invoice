8aeb20d3835f6add305f3dd4e1bf0f9a59633531
OliwiaLakatosz <olakatosz18@gmail.com>
2019-02-09 12:14:48 +0100

    refactoring

--- src/main/java/pl/edu/agh/mwo/invoice/Invoice.java
+++ src/main/java/pl/edu/agh/mwo/invoice/Invoice.java
@@ -4,30 +4,35 @@ import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.Collection;
 
-
 import pl.edu.agh.mwo.invoice.product.Product;
 
 public class Invoice {
 	private Collection<Product> products = new ArrayList<Product>();
+	
+	// Map<Product, Integer>
 
 	public void addProduct(Product product) {
-		// Product(String name, BigDecimal price, BigDecimal tax)
-		products.add(product);
+//		this.products.add(product);
+		this.addProduct(product, 1);
+		// metoda przeciążona
 	}
-
+	
 	public void addProduct(Product product, Integer quantity) {
 //		product.getPrice().multiply(new BigDecimal(quantity));
 //		Product prod = new Product(product.getName(), 
 //				product.getPrice().multiply(new BigDecimal(quantity)), 
 //				product.getTaxPercent());
 //		products.add(prod);
+		for (int i = 0; i < quantity; i++) {
+			this.products.add(product);
+		}
 	}
 		
 
-	public BigDecimal getSubtotal() {
+	public BigDecimal getTotalNetPrice() {
 		BigDecimal subtotal = BigDecimal.ZERO;
-		if (products.isEmpty()) {
-			return subtotal = BigDecimal.ZERO;
+		for (Product product : products) {
+			subtotal = subtotal.add(product.getPrice());
 		}
 		return subtotal;
 	}
